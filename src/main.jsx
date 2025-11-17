db1516e4fe067d8e2fbc5e497a1f0bc9af507685
Valdemar Larsen <shido813@gmail.com>
2025-11-17 09:21:05 +0100

    File structure setup + variables added

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import './styles/main.scss'
+import '@fontsource/lato/400.css';
+import '@fontsource/lato/700.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
