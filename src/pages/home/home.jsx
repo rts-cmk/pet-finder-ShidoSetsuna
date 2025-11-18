71947684351fda106861fad072b22c2f3fc413d9
Valdemar Larsen <shido813@gmail.com>
2025-11-18 17:28:01 +0100

    Added the right shadow effect and added the tags to home!

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,10 +1,12 @@
 import TopBar from "../../components/top_bar/top_bar";
+import TagSelector from "../../components/tag_selector/tag_selector";
 import "./home.scss";
 
 function Home() {
   return (
     <main className="home">
       <TopBar />
+      <TagSelector />
       <h1>Welcome to the Pet App Home Page</h1>
       <p>Find your perfect pet companion here!</p>
     </main>
