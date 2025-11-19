8ef488808f82d727a671d0ffa40e2c820362f0c2
Valdemar Larsen <shido813@gmail.com>
2025-11-19 13:57:08 +0100

    Added icons from react icons  to nav

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,4 +1,8 @@
 import { Link, useLocation } from "react-router";
+import { FiMessageCircle } from "react-icons/fi";
+import { FiHome } from "react-icons/fi";
+import { FiHeart } from "react-icons/fi";
+import { FiUser } from "react-icons/fi";
 import "./nav.scss";
 
 export default function Nav() {
@@ -18,25 +22,7 @@ export default function Nav() {
           <Link
             to="/"
             className={`nav__link ${isActive("/") ? "nav__link--active" : ""}`}>
-            <svg
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M9 22V12h6v10"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
+            <FiHome />
           </Link>
         </li>
         <li className="nav__item">
@@ -45,18 +31,7 @@ export default function Nav() {
             className={`nav__link ${
               isActive("/chat") ? "nav__link--active" : ""
             }`}>
-            <svg
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
+            <FiMessageCircle />
           </Link>
         </li>
         <li className="nav__item">
@@ -65,18 +40,7 @@ export default function Nav() {
             className={`nav__link ${
               isActive("/favourites") ? "nav__link--active" : ""
             }`}>
-            <svg
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
+            <FiHeart />
           </Link>
         </li>
         <li className="nav__item">
@@ -85,27 +49,7 @@ export default function Nav() {
             className={`nav__link ${
               isActive("/profile") ? "nav__link--active" : ""
             }`}>
-            <svg
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <circle
-                cx="12"
-                cy="7"
-                r="4"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
+            <FiUser />
           </Link>
         </li>
       </ul>
